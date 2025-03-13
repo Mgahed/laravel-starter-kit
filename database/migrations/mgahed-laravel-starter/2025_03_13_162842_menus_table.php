bb1a60c5a487b5ab7f0b9e3f4b2d3b62afd83d42
Abdelrhman Mgahed <41325933+Mgahed@users.noreply.github.com>
2025-03-13 17:43:21 +0200

    add menus table and support spatie translate

---
+++ database/migrations/mgahed-laravel-starter/2025_03_13_162842_menus_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+			$table->id();
+			$table->string('ulid')->unique();
+			$table->string('slug')->unique();
+			$table->unsignedBigInteger('parent_id')->nullable();
+			$table->foreign('parent_id')->references('id')->on('menus')->onDelete('cascade');
+			$table->json('title');
+			$table->json('route')->default('#');
+			$table->json('icon')->nullable();
+			$table->integer('record_order')->nullable();
+			$table->string('created_by')->nullable();
+			$table->string('updated_by')->nullable();
+			$table->timestamps();
+			$table->softDeletes();
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
